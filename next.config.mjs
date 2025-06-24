bd44545b6a978f123d169d35daccc3afc31aea78
clickang <clickang_929@naver.com>
2025-06-24 16:39:17 +0900

    gh-pages setting

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  images: { unoptimized: true },
+  basePath: '/clickang.github.io',
+  assetPrefix: '/clickang.github.io/',
 };
 
 export default nextConfig;
